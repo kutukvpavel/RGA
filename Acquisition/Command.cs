2e551d3641782513d93111175947456f6f796aba
kutukvpavel <41666738+kutukvpavel@users.noreply.github.com>
2021-03-12 03:24:57 +0300

    Добавьте файлы проекта.

---
+++ Acquisition/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acquisition
+{
+    public class Command
+    {
+        public Command(string name)
+        {
+            if (name == null) throw new ArgumentNullException();
+            Name = name;
+        }
+
+        public Command(string name, object param) : this(name)
+        {
+            Parameter = param.ToString();
+        }
+
+        public string Name { get; }
+
+        public string Parameter { get; set; }
+
+        public override string ToString()
+        {
+            return Name + (Parameter ?? "");
+        }
+    }
+}
---
+++ Acquisition/Port.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RJCP.IO.Ports;
+
+namespace Acquisition
+{
+    public class Port
+    {
+        public static readonly string NewLine = "\n";
+
+        public event EventHandler<TextEventArgs> LineReceived;
+
+        public Port(SerialPortStream port)
+        {
+            SerialPort = port;
+            port.RtsEnable = true;
+            port.DataReceived += Port_DataReceived;
+        }
+
+        public SerialPortStream SerialPort { get; }
+
+        public void Send(Command cmd)
+        {
+            while (!SerialPort.CtsHolding) ;
+            SerialPort.Write(cmd.ToString());
+            SerialPort.Write(NewLine);
+        }
+
+        private StringBuilder Buffer = new StringBuilder();
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string append = SerialPort.ReadExisting();
+            int index = append.IndexOf(NewLine);
+            if (index > -1)
+            {
+                append = Buffer.Append(append).ToString();
+                while (index > -1)
+                {
+                    LineReceived?.Invoke(this, new TextEventArgs(append.Substring(0, index)));
+                    append.Remove(0, index + 1);
+                    index = append.IndexOf(NewLine);
+                }
+                Buffer.Clear();
+            }
+            Buffer.Append(append);
+        }
+    }
+
+    public class TextEventArgs : EventArgs
+    {
+        public TextEventArgs(string txt)
+        {
+            Text = txt;
+        }
+
+        public string Text { get; }
+    }
+}
